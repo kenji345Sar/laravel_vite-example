216af65db365b8b793ac2474baf5de398c601390
Apple <apple@appurunoMacBook-Pro.local>
2023-06-17 21:25:09 +0900

    route vutify適用

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,14 @@
 import './bootstrap';
 import '../css/app.css';
-import { createApp } from 'vue'
-import App from './App.vue'
-createApp(App).mount("#app")
+import { createApp } from 'vue';
+import App from '../views/App.vue';
+import router from './router';
+import { createVuetify } from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+const app = createApp(App);
+const vuetify = createVuetify();
+app.use(router);
+app.use(vuetify)
+app.mount('#app');
+// createApp(App).use(router).mount("#app")
---
+++ resources/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+        <TopHeader></TopHeader>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import TopHeader from "./TopHeader.vue"
+
+export default {
+    components: {
+        TopHeader
+    }
+}
+</script>
