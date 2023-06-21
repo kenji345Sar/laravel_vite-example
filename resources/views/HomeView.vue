7c4d5ce64a83aee2162ff459ec05594a1c172d2c
Apple <apple@appurunoMacBook-Pro.local>
2023-06-21 20:28:52 +0900

    テーブル対応

--- resources/views/HomeView.vue
+++ resources/views/HomeView.vue
@@ -15,9 +15,9 @@
                 <tbody>
                     <tr v-for="(item, index) in todoList" :key="index">
                         <td>{{ index + 1 }}</td>
-                        <td>{{ item.TODO_NAME }}</td>
-                        <td>{{ item.STATUS }}</td>
-                        <td>{{ item.CREATE_DATE }}</td>
+                        <td>{{ item.title }}</td>
+                        <td>{{ item.status }}</td>
+                        <td>{{ item.create_date }}</td>
                     </tr>
                 </tbody>
             </v-table>
@@ -25,15 +25,27 @@
     </v-container>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-
-// 一覧取得
-const todoList = ref([
-    { TODO_NAME: "タスク1", STATUS: "完了", CREATE_DATE: "2023-06-17" },
-    { TODO_NAME: "タスク2", STATUS: "未完了", CREATE_DATE: "2023-06-16" },
-    { TODO_NAME: "タスク3", STATUS: "完了", CREATE_DATE: "2023-06-15" },
-    { TODO_NAME: "タスク4", STATUS: "未完了", CREATE_DATE: "2023-06-14" }
-])
+<script>
+export default {
+    data() {
+        return {
+            todoList: []
+        }
+    },
+    methods: {
+        getTodoList() {
+            axios.get('/api/todo')
+                .then(response => {
+                    this.todoList = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    },
+    mounted() {
+        this.getTodoList()
+    }
+}
 
 </script>
